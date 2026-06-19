d52dc9698d270b6e11d11784fdfe7c54d683b205
arka.andi.alfiansyah <andi.alfiansyah@arkamaya.co.id>
2026-06-19 17:25:39 +0700

    Migrate Table & Seed
    - buat migrasi table untuk product, order, dan order item
    - buat data dummy untuk memudahkan pengecekan

---
+++ Tugas_1/online-store/database/factories/OrderFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'id' => $this->faker->numberBetween(1, 5),
+            'total_price' => $this->faker->numberBetween(10000, 1000000),
+        ];
+    }
+}
--- Tugas_1/online-store/database/seeders/DatabaseSeeder.php
+++ Tugas_1/online-store/database/seeders/DatabaseSeeder.php
@@ -5,6 +5,10 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Order;
+use App\Models\OrderItem;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,9 +21,30 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        //Buat 20 Product Random menggunakan Factory
+        $products = Product::factory(20)->create();
+
+        //Buat 10 Order menggunakan Factory
+        foreach (range(1, 10) as $index) {
+            $product = $products->random();
+            $order = Order::create([
+                'total_price' => rand(10000, 1000000),
+            ]);
+
+            //Buat OrderItem untuk masing-masing order 1~3
+            $orderProducts = $products->random(rand(1, 3));
+            foreach ($orderProducts as $orderProduct) {
+                OrderItem::create([
+                    'order_id' => $order->id,
+                    'product_id' => $orderProduct->id,
+                    'quantity' => rand(1, 3),
+                ]);
+            }
+        }
     }
 }
